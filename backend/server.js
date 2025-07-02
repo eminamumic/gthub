239699bd72fc3f78428ac86157c539992800d6eb
eminamumic <emimumic1@gmail.com>
2025-07-02 18:03:35 +0200

    Add server.js et backend

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const db = require('./config/db')
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('GTHub Admin Backend API is running!')
+})
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const [rows] = await db.query('SELECT 1 + 1 AS solution')
+    res.json({
+      message: 'Database query successful!',
+      solution: rows[0].solution,
+    })
+  } catch (error) {
+    console.error('Error executing DB query:', error)
+    res
+      .status(500)
+      .json({ message: 'Database query failed!', error: error.message })
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+  console.log(`Access at: http://localhost:${PORT}`)
+})
