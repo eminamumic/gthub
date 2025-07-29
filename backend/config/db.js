2e8d626f9335ab20fddc27c6216bdfc8a1fb00f9
eminamumic <emimumic1@gmail.com>
2025-07-29 13:36:52 +0200

    Fix server.js file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,34 +1,25 @@
+const mysql = require('mysql2/promise')
 require('dotenv').config()
-
-const mysql = require('mysql2')
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
   waitForConnections: true,
 })
 
-pool.getConnection((err, connection) => {
-  if (err) {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.error('Database connection was closed.')
-    }
-    if (err.code === 'ER_CON_COUNT_ERROR') {
-      console.error('Database has too many connections.')
-    }
-    if (err.code === 'ECONNREFUSED') {
-      console.error(
-        'Database connection was refused. Check your DB credentials or if the MySQL server is running.'
-      )
-    }
-    console.error('Error connecting to the database:', err.message)
-    return
+async function testDbConnection() {
+  try {
+    const connection = await pool.getConnection()
+    console.log('Successfully connected to the database!')
+    connection.release()
+  } catch (error) {
+    console.error('Error connecting to the database:', error)
+    process.exit(1)
   }
-  console.log('Successfully connected to the database!')
-  connection.release()
-})
+}
 
-module.exports = pool.promise()
+testDbConnection()
+
+module.exports = pool
