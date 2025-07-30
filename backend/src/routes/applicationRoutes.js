9434c9e9300dc5ab12cb5817bbff9127ab11448d
eminamumic <emimumic1@gmail.com>
2025-07-30 22:49:51 +0200

    Add application routes

---
+++ backend/src/routes/applicationRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const applicationsController = require('../controllers/applicationsController')
+const authMiddleware = require('../middleware/authMiddleware')
+
+const router = express.Router()
+
+router.use(authMiddleware.authenticateToken)
+
+router.get('/', applicationsController.getApplications)
+
+router.get('/:id', applicationsController.getApplication)
+
+router.post('/', applicationsController.addApplication)
+
+router.delete('/:id', applicationsController.deleteApplication)
+
+module.exports = router
