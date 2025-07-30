c1a8ac54060f88a99aaa5b1b8cdeba2e78200640
eminamumic <emimumic1@gmail.com>
2025-07-30 21:26:26 +0200

    Add authRoutes to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const authRoutes = require('./src/routes/authRoutes')
 require('dotenv').config()
 
 const db = require('./src/config/db')
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/auth', authRoutes)
 
 app.get('/', (req, res) => {
   res.send('GTHub Admin Backend je pokrenut!')
