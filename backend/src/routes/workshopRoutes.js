6ba5c012ec4278e936ee16d89d6185203698b9cd
eminamumic <emimumic1@gmail.com>
2025-07-30 22:35:54 +0200

    Add route for workshops

--- backend/src/routes/workshopRoutes.js
+++ backend/src/routes/workshopRoutes.js
@@ -1,14 +1,19 @@
 const express = require('express')
-const {
-  getWorkshops,
-  createWorkshop,
-} = require('../controllers/workshopController')
-const { protect } = require('../middleware/authMiddleware')
+const workshopsController = require('../controllers/workshopsController')
+const authMiddleware = require('../middleware/authMiddleware')
 
 const router = express.Router()
 
-router.get('/', protect, getWorkshops)
+router.use(authMiddleware.authenticateToken)
 
-router.post('/', protect, createWorkshop)
+router.get('/', workshopsController.getWorkshops)
+
+router.get('/:id', workshopsController.getWorkshop)
+
+router.post('/', workshopsController.addWorkshop)
+
+router.put('/:id', workshopsController.updateWorkshop)
+
+router.delete('/:id', workshopsController.deleteWorkshop)
 
 module.exports = router
