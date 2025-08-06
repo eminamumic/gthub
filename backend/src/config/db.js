b365f35b8ab8aafd3fde1aabfe74e4c6d61962dc
eminamumic <emimumic1@gmail.com>
2025-08-06 13:10:30 +0200

    Fix db bug

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,21 +3,21 @@ require('dotenv').config()
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  user: process.env.DB_USERNAME, // Promijenjeno u DB_USERNAME
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE, // Promijenjeno u DB_DATABASE
   waitForConnections: true,
+  port: process.env.DB_PORT, // Dodano
 })
 
 pool
   .getConnection()
   .then((connection) => {
     console.log('Uspješno povezan na MySQL bazu podataka!')
-    connection.release()
+    connection.release() // Bitno je osloboditi konekciju nakon provjere
   })
   .catch((err) => {
     console.error('Greška pri konekciji na MySQL bazu podataka:', err.message)
-    process.exit(1)
   })
 
 module.exports = pool
