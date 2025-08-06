e0092bd2d922869c84de7570060e4ad3b630916d
eminamumic <emimumic1@gmail.com>
2025-08-06 13:24:09 +0200

    Add global css

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,18 +3,18 @@ require('dotenv').config()
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME, // Promijenjeno u DB_USERNAME
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE, // Promijenjeno u DB_DATABASE
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
-  port: process.env.DB_PORT, // Dodano
+  port: process.env.DB_PORT,
 })
 
 pool
   .getConnection()
   .then((connection) => {
     console.log('Uspješno povezan na MySQL bazu podataka!')
-    connection.release() // Bitno je osloboditi konekciju nakon provjere
+    connection.release()
   })
   .catch((err) => {
     console.error('Greška pri konekciji na MySQL bazu podataka:', err.message)
