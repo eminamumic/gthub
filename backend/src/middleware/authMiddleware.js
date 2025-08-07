7b48ae3bc7a9e1423e6a4c254899be56890f5a7a
eminamumic <emimumic1@gmail.com>
2025-08-07 14:17:39 +0200

    Change gtom bosnian to english

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -8,12 +8,12 @@ function authenticateToken(req, res, next) {
   if (!token) {
     return res
       .status(401)
-      .json({ message: 'Pristup odbijen. Token nije dostavljen.' })
+      .json({ message: 'Access denied. Token not provided.' })
   }
 
   jwt.verify(token, jwtSecret, (err, user) => {
     if (err) {
-      return res.status(403).json({ message: 'Nevažeći ili istekao token.' })
+      return res.status(403).json({ message: 'Invalid or expired token.' })
     }
 
     req.user = user
